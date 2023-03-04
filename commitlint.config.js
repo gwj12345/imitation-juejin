e30f745921980c0ff1baf5dca06606414adfe9ec
gwj1139 <gwj1024@qq.com>
2023-03-04 21:27:46 +0800

    docs: 增加commitlint注释

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,16 @@ module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
       'type-case': [2, 'always', ['lower-case', 'upper-case']],
-      'type-enum': [2, 'always',['feat', 'fix', 'docs','style','refactor','perf','test', 'chore', 'revert']]
+      'type-enum': [2, 'always',[
+		  'feat',    //新增功能
+		  'fix',     //修复bug
+		  'docs',    //文档变更
+		  'style',   //代码格式
+		  'refactor',//代码重构
+		  'perf',    //性能优化
+		  'test',    //添加疏漏测试
+		  'chore',   //对构建过程或辅助工具和库的更改
+		  'revert'   //回滚 commit
+	  ]]
     }
   }
