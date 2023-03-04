8e61e4bf476eff86d5da5dd2019514ac52908385
gwj1139 <gwj1024@qq.com>
2023-03-04 21:13:27 +0800

    feat: 增加husky和commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-case': [2, 'always', ['lower-case', 'upper-case']],
+      'type-enum': [2, 'always',['feat', 'fix', 'docs','style','refactor','perf','test', 'chore', 'revert']]
+    }
+  }
